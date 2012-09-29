2708e6bf24db5f270d6886e435e07cd7ca5c0015
Florin Dan Munteanu <florin.dan.munteanu@gmail.com>
2012-09-29 15:19:52 +0300

    Weighted quick union

---
+++ coursera.princeton.algorithms.part1/src/week1/WeightedQuickUnion.java
@@ -0,0 +1,58 @@
+package week1;
+
+public class WeightedQuickUnion {
+
+	private int[] id;
+	
+	private int[] size;
+	
+	public WeightedQuickUnion(int N)
+	{
+		this.id = new int[N];
+		this.size = new int[N];
+		for (int i = 0; i < N; i++)
+		{
+			//every node is a root
+			this.id[i] = i;
+			//size of each tree is 1
+			this.size[i] = 1;
+		}
+	}
+	
+	private int root(int i)
+	{
+		// get the root of node i
+		// navigate all the way to the root
+		while(i != this.id[i])
+		{
+			// path compression
+			// make every other node in path
+			// point to its grandparent
+			// id[i] = id[id[i]];
+			i = this.id[i];
+		}
+		return i;
+	}
+	
+	public boolean connected(int p, int q)
+	{
+		return this.root(p) == this.root(q);
+	}
+	
+	public void union(int p, int q)
+	{
+		int i = this.root(p);
+		int j = this.root(q);
+		
+		if (this.size[i] < this.size[j])
+		{
+			this.id[i] = j;
+			this.size[j] += this.size[i];
+		}
+		else
+		{
+			this.id[j] = i;
+			this.size[i] += this.size[j];
+		}
+	}
+}
