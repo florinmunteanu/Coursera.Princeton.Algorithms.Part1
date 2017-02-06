0e9aa7f8d505f8491c233167f3e3a9076885d1ca
Florin Dan Munteanu <florin.dan.munteanu@gmail.com>
2017-02-06 17:18:18 +0200

    Update BinarySearchTree.java

--- coursera.princeton.algorithms.part1/src/trees/BinarySearchTree.java
+++ coursera.princeton.algorithms.part1/src/trees/BinarySearchTree.java
@@ -290,6 +290,30 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value>
 		}
 	}
 	
+    public void delete(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to delete() is null");
+        root = delete(root, key);       
+    }
+
+    private Node delete(Node x, Key key) 
+    {
+        if (x == null) return null;
+
+        int cmp = key.compareTo(x.key);
+        if      (cmp < 0) x.left  = delete(x.left,  key);
+        else if (cmp > 0) x.right = delete(x.right, key);
+        else { 
+            if (x.right == null) return x.left;
+            if (x.left  == null) return x.right;
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        } 
+        x.size = size(x.left) + size(x.right) + 1;
+        return x;
+    } 
+	
 	private void displayTree(Node root)
 	{
 		if (root == null) { return; }
